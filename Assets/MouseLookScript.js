139207e9b66addb48af9587b44288dd4fb8cf700
TORCHE Guillaume <gtorche@gmail.com>
2014-02-11 12:21:07 +0100

    First commit

---
+++ Assets/MouseLookScript.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var lookSensitivity : float = 5;
+// Permet de supprimer les variables de l'inspector (On a pas besoin de les modifier depuis l'inspector manuellement)
+@HideInInspector
+var xRotation : float;
+@HideInInspector
+var yRotation : float;
+@HideInInspector
+var currentYRotation : float;
+@HideInInspector
+var currentXRotation : float;
+@HideInInspector
+var yRotationV : float;
+@HideInInspector
+var xRotationV : float;
+var lookSmoothDamp : float = 0.1;
+
+function Update () {
+	
+	yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+	xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+	
+	xRotation = Mathf.Clamp(xRotation, -90, 90);
+	
+	// Permet de ne pas avoir un effet trop "brut" lors du déplacement de la caméra
+	currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, xRotationV, lookSmoothDamp);
+	currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, yRotationV, lookSmoothDamp);
+	
+	transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+}
